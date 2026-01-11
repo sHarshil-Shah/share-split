08c76df58fbe93da9fa659cbc0d56726bc53dedd
Harshil Shah <35626302+sHarshil-Shah@users.noreply.github.com>
2026-01-11 22:11:15 +0000

    items updated

--- item.py
+++ item.py
@@ -1,59 +1,92 @@
 from enum import Enum
 
-
 class PERSONS(Enum):
     HARSHIL = 1
-    PARTH = 2
-    FORAM = 3
-    PARTH_P = 4
-    SOUTH = 5
-
+    MANOJ = 2
+    MAITRI = 3
+    SAKSHI = 4
 
 items = [
   {
-    "name": "Pears Gentle Care Transparent Soap Regular",
-    "value": 10.71,
-    "taxable": true, 
-    "people": [PERSONS.HARSHIL] 
+    "name": "Pepper, Green Bell",
+    "value": 1.85,
+    "taxable": False,
+    "people": [PERSONS.MANOJ, PERSONS.MAITRI, PERSONS.SAKSHI]
   },
   {
-    "name": "Lime Regular",
-    "value": 1.12,
-    "taxable": false, 
-    "people": [PERSONS.HARSHIL, PERSONS.PARTH, PERSONS.PARTH_P] 
+    "name": "Eggplant, Large",
+    "value": 7.78,
+    "taxable": False,
+    "people": [PERSONS.MANOJ, PERSONS.MAITRI, PERSONS.SAKSHI, PERSONS.HARSHIL]
   },
   {
-    "name": "Proctor Silex Iron with Stainless Steel Soleplate 17172PS Regular",
-    "value": 16.96,
-    "taxable": true, 
-    "people": [PERSONS.HARSHIL, PERSONS.PARTH_P] 
+    "name": "Tomato, Roma",
+    "value": 3.93,
+    "taxable": False,
+    "people": [PERSONS.MANOJ, PERSONS.MAITRI, PERSONS.SAKSHI]
   },
   {
-    "name": "Sealtest Homogenized 3.25% Milk Regular",
-    "value": 21.21,
-    "taxable": false,
-    "people": [PERSONS.HARSHIL, PERSONS.PARTH, PERSONS.PARTH_P, PERSONS.SOUTH] 
+    "name": "Tomato, Roma",
+    "value": 3.93,
+    "taxable": False,
+    "people": [PERSONS.HARSHIL]
   },
   {
-    "name": "Head & Shoulders Deep Scalp Cleanse 2-in-1 Anti-Dandruff Shampoo + Conditioner Regular",
-    "value": 6.97,
-    "taxable": true, 
-    "people": [PERSONS.PARTH_P] 
+    "name": "Sealtest Homogenized 3.25% Milk, 4 L",
+    "value": 5.81,
+    "taxable": False,
+    "people": [PERSONS.MANOJ, PERSONS.MAITRI, PERSONS.SAKSHI, PERSONS.HARSHIL]
   },
   {
-    "name": "Tomato, Roma Weight-Adjusted",
-    "value": 5.00,
-    "taxable": false,
-    "people": [PERSONS.HARSHIL, PERSONS.PARTH, PERSONS.PARTH_P] 
+    "name": "ReaLemon Lemon Juice, 945 mL",
+    "value": 2.79,
+    "taxable": False,
+    "people": [PERSONS.MANOJ, PERSONS.MAITRI, PERSONS.SAKSHI]
   },
   {
-    "name": "Banana Weight-Adjusted",
-    "value": 1.91,
-    "taxable": false,
-    "people": [PERSONS.HARSHIL, PERSONS.PARTH_P] 
+    "name": "Sealtest 35% Whipping Cream, 473 mL",
+    "value": 3.99,
+    "taxable": False,
+    "people": [PERSONS.MANOJ, PERSONS.MAITRI, PERSONS.SAKSHI]
+  },
+  {
+    "name": "Becel Margarine Original, 427g",
+    "value": 3.99,
+    "taxable": False,
+    "people": [PERSONS.MANOJ, PERSONS.MAITRI, PERSONS.SAKSHI]
+  },
+  {
+    "name": "Fairlife Chocolate Milk 2% Bottle, 1.5 L",
+    "value": 5.32,
+    "taxable": False,
+    "people": [PERSONS.MANOJ, PERSONS.MAITRI, PERSONS.SAKSHI]
+  },
+  {
+    "name": "Gits Khatta Dhokla Instant Mix (Qty 4)",
+    "value": 5.64,
+    "taxable": False,
+    "people": [PERSONS.MANOJ, PERSONS.MAITRI, PERSONS.SAKSHI, PERSONS.HARSHIL]
+  },
+  {
+    "name": "Febreze Plug In Air Freshener, Ocean Scent",
+    "value": 12.96,
+    "taxable": True, 
+    "people": [PERSONS.MANOJ, PERSONS.MAITRI, PERSONS.SAKSHI, PERSONS.HARSHIL]
+  },
+  {
+    "name": "Balderson 2 Year Old Cheddar Cheese, 500g",
+    "value": 15.60,
+    "taxable": False,
+    "people": [PERSONS.MANOJ, PERSONS.MAITRI, PERSONS.SAKSHI]
+  },
+  {
+    "name": "Lijjat Jeera Papad, 200g",
+    "value": 1.42,
+    "taxable": False,
+    "people": [PERSONS.MANOJ, PERSONS.MAITRI, PERSONS.SAKSHI, PERSONS.HARSHIL]
   }
 ]
 
 
-tax_rate = 13  # 15% tax rate
-total_discount = 14.87  # total discount
+tax_rate = 13  # 13% tax rate
+total_discount = 0  # total discount
